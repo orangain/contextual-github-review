29cf3b4d0e6aebcf4df1f7778505b199ebbdad07
orangain <orangain@gmail.com>
2025-06-22 15:59:12 +0900

    Remove unused code

--- background.js
+++ background.js
@@ -1,23 +1,10 @@
 chrome.runtime.onInstalled.addListener(() => {
-  console.log('GitHub PR Blame Viewer extension installed');
-});
-
-// Handle extension icon click
-chrome.action.onClicked.addListener((tab) => {
-  if (tab.url && tab.url.includes('github.com') && tab.url.includes('/pull/')) {
-    chrome.tabs.sendMessage(tab.id, { action: 'toggle_blame_display' });
-  }
+  console.log('Contextual GitHub Review extension installed');
 });
 
 // Handle messages from content script
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   switch (request.action) {
-    case 'get_github_token': {
-      chrome.storage.sync.get(['github_token'], (result) => {
-        sendResponse({ token: result.github_token });
-      });
-      return true;
-    }
     case 'fetch_blame_data': {
       const { args } = request;
       console.log('Received fetch_blame_data request with args:', args);
